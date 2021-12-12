2dc61161386773021e40e5d96a59a88744e88837
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-12-11 22:47:59 -0700

    changed a few things on the roles table

--- db/schema.sql
+++ db/schema.sql
@@ -4,19 +4,23 @@ DROP DATABASE IF EXISTS store_db;
 CREATE DATABASE store_db;
 
 USE store_db;
-
+--  drops all the tables if they exist then creates the tables
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employee;
 -- A table is composed of rows and columns. A column represents a field. A row represents a record.
 CREATE TABLE department(
   id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
   department_name VARCHAR(30) NOT NULL
 );
-
+-- A foreign key is a field in one table that references the primary key of another table
 CREATE TABLE roles(
   id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(14,2) NOT NULL,
   role_id INTEGER NOT NULL,
-  FOREIGN KEY (role_id) REFERENCES department(id)
+  department_id INTEGER
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee(
@@ -27,3 +31,13 @@ CREATE TABLE employee(
   FOREIGN KEY (role_id) REFERENCES roles(id),
   manager_id INTEGER NULL
 );
+
+
+-- RUN in the command line in mysql shell
+-- source db/schema.sql
+-- source db/seeds.sql
+
+-- RUN a few queries
+-- SELECT * FROM department;
+-- SELECT * FROM roles;
+-- SELECT * FROM employee;
